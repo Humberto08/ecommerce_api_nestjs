4a48fad7e0b3ab2de2e0ff7a46ebe2a242697879
Humberto Luciano <humberto_luciano@hotmail.com>
2024-08-27 16:44:07 -0300

    feat: create user dto and route create user (post)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    async getAllUsers() {
-        return JSON.stringify({ test: "123" });
-    }
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return {
+      ...createUser,
+      password: undefined,
+    };
+  }
 }
