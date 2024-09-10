8ade5c5de3400ec578893a87dfc005a4dcbb1866
Humberto Luciano <humberto_luciano@hotmail.com>
2024-09-10 12:49:12 -0300

    feat: inseart relation many-to-one

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-
+import { AddressEntity } from "src/address/entities/address.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({ name: 'user' })
 export class UserEntity {
@@ -25,9 +24,12 @@ export class UserEntity {
   @Column({ name: 'type_user', nullable: false })
   typeUser: number;
 
-  @Column({ name: 'created_at' })
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
-  @Column({ name: 'updated_at' })
+  @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
+
+  @OneToMany(() => AddressEntity, (address) => address.user)
+  addresses?: AddressEntity[]
 }
