438799bcf81b370e3d97fc20a538674f21a2fe99
Humberto Luciano <humberto_luciano@hotmail.com>
2024-09-20 16:14:08 -0300

    feat: create function createCategory using TDD

--- src/category/__tests__/category.service.spec.ts
+++ src/category/__tests__/category.service.spec.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CategoryEntity } from '../entities/category.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { categoryMock } from '../__mocks__/category.mock';
+import { createCategoryMock } from '../__mocks__/create-category.mock';
 
 describe('CategoryService', () => {
   let service: CategoryService;
@@ -11,19 +12,23 @@ describe('CategoryService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService, {
-        provide: getRepositoryToken(CategoryEntity),
-        useValue: {
-          find: jest.fn().mockResolvedValue([categoryMock]),
-          save: jest.fn().mockResolvedValue([categoryMock]),
+      providers: [
+        CategoryService,
+        {
+          provide: getRepositoryToken(CategoryEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(categoryMock),
+            find: jest.fn().mockResolvedValue([categoryMock]),
+            save: jest.fn().mockResolvedValue([categoryMock]),
+          },
         },
-      }],
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
     categoryRepository = module.get<Repository<CategoryEntity>>(
       getRepositoryToken(CategoryEntity),
-    )
+    );
   });
 
   it('should be defined', () => {
@@ -34,19 +39,44 @@ describe('CategoryService', () => {
   it('should return list category', async () => {
     const categories = await service.findAllCategories();
 
-    expect(categories).toEqual([categoryMock]) 
+    expect(categories).toEqual([categoryMock]);
   });
 
   it('should return error in list category empty', async () => {
     jest.spyOn(categoryRepository, 'find').mockResolvedValueOnce([]);
 
     expect(service.findAllCategories()).rejects.toThrow();
-});
+  });
 
   it('should return error in list category exception', async () => {
     jest.spyOn(categoryRepository, 'find').mockRejectedValue(new Error());
 
     expect(service.findAllCategories()).rejects.toThrow();
+  });
 
+  it('should return category after save', async () => {
+    jest.spyOn(categoryRepository, 'findOne').mockResolvedValue(undefined);
+
+    const category = await service.createCategory(createCategoryMock);
+
+    expect(category).toEqual([categoryMock]);
+  });
+
+  it('should return error in exception', async () => {
+    jest.spyOn(categoryRepository, 'save').mockRejectedValue(new Error());
+
+    expect(service.createCategory(createCategoryMock)).rejects.toThrow();
+  });
+
+  it('should return category in find by name', async () => {
+    const category = await service.findCategoryByName(categoryMock.name);
+
+    expect(category).toEqual(categoryMock);
+  });
+
+  it('should return error if category in find by name empty', async () => {
+    jest.spyOn(categoryRepository, 'findOne').mockResolvedValueOnce(undefined);
+
+    expect(service.findCategoryByName(categoryMock.name)).rejects.toThrow();
   });
 });
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CategoryEntity } from './entities/category.entity';
+import { CreateCategory } from './dtos/create-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -18,5 +19,31 @@ export class CategoryService {
     }
 
     return categories;
+  }
+
+  async findCategoryByName(name: string): Promise<CategoryEntity> {
+    const category = await this.categoryRepository.findOne({
+      where: { name },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category ${name} not found`);
+    }
+
+    return category;
+  }
+
+  async createCategory(
+    createCategory: CreateCategory,
+  ): Promise<CategoryEntity> {
+    const category = await this.findCategoryByName(createCategory.name).catch(
+      () => undefined,
+    );
+
+    if (category) {
+      throw new NotFoundException(`Category ${createCategory.name} already exists`);
+    }
+
+    return this.categoryRepository.save(createCategory);
   }
 }
