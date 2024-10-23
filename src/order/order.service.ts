96180ee63858d91457948ccc69480acd8953cbe6
Humberto Luciano <humberto_luciano@hotmail.com>
2024-10-23 14:41:48 -0300

    feat: create Payment service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,15 +3,19 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { CreateOrderDto } from './dtos/create-order.dto';
+import { PaymentService } from '../payment/payment.service';
+
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(OrderEntity)
     private readonly orderRepository: Repository<OrderEntity>,
+    private readonly paymentService: PaymentService,
   ) {}
 
   async createOrder(createOrderDto: CreateOrderDto, cartId: number) {
+    await this.paymentService.createPayment(createOrderDto);
     return null;
   }
 }
