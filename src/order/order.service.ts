37df5390c1e5cc13c23f8728626be0f1347aec10
Humberto Luciano <humberto_luciano@hotmail.com>
2024-10-29 08:42:50 -0300

    feat: create orderProductService

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -4,6 +4,9 @@ import { Repository } from 'typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { CreateOrderDto } from './dtos/create-order.dto';
 import { PaymentService } from '../payment/payment.service';
+import { PaymentEntity } from '../payment/entities/payment.entity';
+import { CartService } from '../cart/cart.service';
+import { OrderProductService } from '../order-product/order-product.service';
 
 
 @Injectable()
@@ -12,10 +15,32 @@ export class OrderService {
     @InjectRepository(OrderEntity)
     private readonly orderRepository: Repository<OrderEntity>,
     private readonly paymentService: PaymentService,
+    private readonly cartService: CartService,
+    private readonly orderProductService: OrderProductService
   ) {}
 
-  async createOrder(createOrderDto: CreateOrderDto, cartId: number) {
-    await this.paymentService.createPayment(createOrderDto);
+  async createOrder(createOrderDto: CreateOrderDto, cartId: number, userId: number) {
+    const payment: PaymentEntity = await this.paymentService.createPayment(createOrderDto);
+
+  const order = await this.orderRepository.save({
+    addressId: createOrderDto.addressId,
+    date: new Date(),
+    paymentId: payment.id,
+    userId,
+  });
+
+  const cart = await this.cartService.findCartByUserId(userId, true);
+
+  cart.cartProduct?.forEach((cartProduct) => {
+    this.orderProductService.createOrderProduct(
+      cartProduct.productId,
+      order.id,
+      0,
+      cartProduct.amount,
+
+    )
+  })
+
     return null;
   }
 }
