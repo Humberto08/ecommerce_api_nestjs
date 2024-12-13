ac85f329577fa14cd8ca73d055e0386c10a61094
Humberto Luciano <humberto_luciano@hotmail.com>
2024-12-13 14:52:22 -0300

    feat: insert info to delivery in insert and put product

--- src/product/dtos/update-procut.dto.ts
+++ src/product/dtos/update-procut.dto.ts
@@ -12,4 +12,19 @@ export class UpdateProductDto {
 
   @IsString()
   image: string;
+
+  @IsNumber()
+  weight?: number;
+
+  @IsNumber()
+  length?: number;
+
+  @IsNumber()
+  height?: number;
+
+  @IsNumber()
+  width?: number;
+  
+  @IsNumber()
+  diameter?: number;
 }
